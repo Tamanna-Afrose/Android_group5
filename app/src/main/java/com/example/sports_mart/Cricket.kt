31edb918a746441e5fb1ea9c76cd1976cebcd7ea
arnab-shanta-anu <arnab.shanta@bjitacademy.com>
2022-11-25 17:24:45 +0600

    latest

--- app/src/main/java/com/example/sports_mart/Cricket.kt
+++ app/src/main/java/com/example/sports_mart/Cricket.kt
@@ -1,11 +1,38 @@
 package com.example.sports_mart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sports_mart.databinding.ActivityCricketBinding
 
 class Cricket : AppCompatActivity() {
+    private lateinit var binding: ActivityCricketBinding
+    private var quantity: Int = 1
+    private var productCost: Int = 200
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cricket)
+        binding = ActivityCricketBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.minusButton.setOnClickListener {
+            quantity--
+            if (quantity <= 0) {
+                Log.d("Cricket", "quantity is less than 0")
+                val toast = Toast.makeText(
+                    applicationContext, "quantity can't be less than 1", Toast.LENGTH_SHORT
+                )
+                quantity = 1
+            }
+            binding.quantity.text = quantity.toString()
+        }
+        binding.plusButton.setOnClickListener {
+            quantity++
+            binding.quantity.text = quantity.toString()
+        }
+        binding.bill.setOnClickListener{
+            if (binding.confirm.isChecked) {
+                binding.cost.text = (quantity * productCost).toString()
+            }
+        }
     }
 }
