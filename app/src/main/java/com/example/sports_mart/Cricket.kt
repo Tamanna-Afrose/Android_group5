3d26d648650b9dd20d362cd910df24aec6ce8631
Tamanna-Afrose <tamanna.afrose@bjitacademy.com>
2022-11-25 14:56:24 +0600

    new activity cricket added & scroll view correct

---
+++ app/src/main/java/com/example/sports_mart/Cricket.kt
@@ -0,0 +1,11 @@
+package com.example.sports_mart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cricket : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cricket)
+    }
+}
