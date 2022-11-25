13e77841863e747516921ee871bac3c7aa79f8a8
Tamanna-Afrose <tamanna.afrose@bjitacademy.com>
2022-11-25 14:37:40 +0600

    new activity sports added

--- app/src/main/java/com/example/sports_mart/MainActivity.kt
+++ app/src/main/java/com/example/sports_mart/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.sports_mart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sports_mart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.shopButton.setOnClickListener {
+            val intent = Intent(this, Soprts::class.java)
+
+            startActivity(intent)
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/example/sports_mart/Soprts.kt
@@ -0,0 +1,11 @@
+package com.example.sports_mart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Soprts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soprts)
+    }
+}
