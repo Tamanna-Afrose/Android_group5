d0fa5ee0ac936d13c60bee34438bf28fd0c09812
Tamanna-Afrose <tamanna.afrose@bjitacademy.com>
2022-11-25 15:54:24 +0600

    cricket window added

--- app/src/main/java/com/example/sports_mart/Soprts.kt
+++ app/src/main/java/com/example/sports_mart/Soprts.kt
@@ -1,11 +1,20 @@
 package com.example.sports_mart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sports_mart.databinding.ActivitySoprtsBinding
 
 class Soprts : AppCompatActivity() {
+    private lateinit var binding:ActivitySoprtsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_soprts)
+        binding = ActivitySoprtsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.cricket.setOnClickListener {
+            val intent = Intent(this, Cricket::class.java)
+
+            startActivity(intent)
+        }
     }
 }
