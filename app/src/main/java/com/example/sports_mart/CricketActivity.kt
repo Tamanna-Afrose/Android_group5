c96b663dee56af3bb79e13c6efd97a0a75c7052e
tamannaafrose09@gmail.com <50029279+FM-Man@users.noreply.github.com>
2022-11-28 01:02:03 +0600

    functionality added in cricket window gloves

--- app/src/main/java/com/example/sports_mart/CricketActivity.kt
+++ app/src/main/java/com/example/sports_mart/CricketActivity.kt
@@ -6,45 +6,25 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sports_mart.databinding.ActivityCricketBinding
-
+var quantity: Int=1
+var productCost:Int=0
+var totalCost:Int =0
 class CricketActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCricketBinding
-    private var quantity: Int = 1
-    private var productCost: Int = 200
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCricketBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.minusButton.setOnClickListener {
-            quantity--
-            if (quantity <= 0) {
-                Log.d("Cricket", "quantity is less than 0")
-                val toast = Toast.makeText(
-                    applicationContext, R.string.quantityToast, Toast.LENGTH_SHORT
-                )
-                quantity = 1
-            }
-            binding.quantity.text = quantity.toString()
-        }
-        binding.plusButton.setOnClickListener {
-            quantity++
-            binding.quantity.text = quantity.toString()
-        }
-        binding.bill.setOnClickListener{
-            if (binding.confirm.isChecked) {
-                binding.cost.text = (quantity * productCost).toString()
-            }
-        }
         binding.golveText.setOnClickListener {
             var builder = AlertDialog.Builder(this)
             builder.setTitle(R.string.dialog_title)
             builder.setMessage(R.string.dialogMessage)
             builder.setIcon(android.R.drawable.ic_dialog_info)
-            builder.setPositiveButton(R.string.addedItem){dialogInterface, which ->
-                Toast.makeText(applicationContext,"Item Added",Toast.LENGTH_LONG).show()
+            builder.setPositiveButton(R.string.addedItem) { dialogInterface, which ->
+                Toast.makeText(applicationContext, "Item Added", Toast.LENGTH_LONG).show()
             }
-            builder.setNeutralButton("Cancel"){dialogInterface , which ->
-                Toast.makeText(applicationContext,R.string.cancelString,Toast.LENGTH_LONG).show()
+            builder.setNeutralButton("Cancel") { dialogInterface, which ->
+                Toast.makeText(applicationContext, R.string.cancelString, Toast.LENGTH_LONG).show()
             }
             val alertDialog: AlertDialog = builder.create()
             alertDialog.setCancelable(false)
@@ -55,11 +35,11 @@ class CricketActivity : AppCompatActivity() {
             builder.setTitle(R.string.helmetTitle)
             builder.setMessage(R.string.helmet_details)
             builder.setIcon(android.R.drawable.ic_dialog_info)
-            builder.setPositiveButton(R.string.addedItem){dialogInterface, which ->
-                Toast.makeText(applicationContext,"Item Added",Toast.LENGTH_LONG).show()
+            builder.setPositiveButton(R.string.addedItem) { dialogInterface, which ->
+                Toast.makeText(applicationContext, "Item Added", Toast.LENGTH_LONG).show()
             }
-            builder.setNeutralButton("Cancel"){dialogInterface , which ->
-                Toast.makeText(applicationContext,R.string.cancelString,Toast.LENGTH_LONG).show()
+            builder.setNeutralButton("Cancel") { dialogInterface, which ->
+                Toast.makeText(applicationContext, R.string.cancelString, Toast.LENGTH_LONG).show()
             }
             val alertDialog: AlertDialog = builder.create()
             alertDialog.setCancelable(false)
@@ -70,11 +50,11 @@ class CricketActivity : AppCompatActivity() {
             builder.setTitle(R.string.batTitle)
             builder.setMessage(R.string.bat_details)
             builder.setIcon(android.R.drawable.ic_dialog_info)
-            builder.setPositiveButton(R.string.addedItem){dialogInterface, which ->
-                Toast.makeText(applicationContext,"Item Added",Toast.LENGTH_LONG).show()
+            builder.setPositiveButton(R.string.addedItem) { dialogInterface, which ->
+                Toast.makeText(applicationContext, "Item Added", Toast.LENGTH_LONG).show()
             }
-            builder.setNeutralButton("Cancel"){dialogInterface , which ->
-                Toast.makeText(applicationContext,R.string.cancelString,Toast.LENGTH_LONG).show()
+            builder.setNeutralButton("Cancel") { dialogInterface, which ->
+                Toast.makeText(applicationContext, R.string.cancelString, Toast.LENGTH_LONG).show()
             }
             val alertDialog: AlertDialog = builder.create()
             alertDialog.setCancelable(false)
@@ -85,11 +65,11 @@ class CricketActivity : AppCompatActivity() {
             builder.setTitle(R.string.ballTitle)
             builder.setMessage(R.string.bat_details)
             builder.setIcon(android.R.drawable.ic_dialog_info)
-            builder.setPositiveButton(R.string.addedItem){dialogInterface, which ->
-                Toast.makeText(applicationContext,"Item Added",Toast.LENGTH_LONG).show()
+            builder.setPositiveButton(R.string.addedItem) { dialogInterface, which ->
+                Toast.makeText(applicationContext, "Item Added", Toast.LENGTH_LONG).show()
             }
-            builder.setNeutralButton("Cancel"){dialogInterface , which ->
-                Toast.makeText(applicationContext,R.string.cancelString,Toast.LENGTH_LONG).show()
+            builder.setNeutralButton("Cancel") { dialogInterface, which ->
+                Toast.makeText(applicationContext, R.string.cancelString, Toast.LENGTH_LONG).show()
             }
             val alertDialog: AlertDialog = builder.create()
             alertDialog.setCancelable(false)
@@ -100,11 +80,11 @@ class CricketActivity : AppCompatActivity() {
             builder.setTitle(R.string.stampTitle)
             builder.setMessage(R.string.stamp_details)
             builder.setIcon(android.R.drawable.ic_dialog_info)
-            builder.setPositiveButton(R.string.addedItem){dialogInterface, which ->
-                Toast.makeText(applicationContext,"Item Added",Toast.LENGTH_LONG).show()
+            builder.setPositiveButton(R.string.addedItem) { dialogInterface, which ->
+                Toast.makeText(applicationContext, "Item Added", Toast.LENGTH_LONG).show()
             }
-            builder.setNeutralButton("Cancel"){dialogInterface , which ->
-                Toast.makeText(applicationContext,R.string.cancelString,Toast.LENGTH_LONG).show()
+            builder.setNeutralButton("Cancel") { dialogInterface, which ->
+                Toast.makeText(applicationContext, R.string.cancelString, Toast.LENGTH_LONG).show()
             }
             val alertDialog: AlertDialog = builder.create()
             alertDialog.setCancelable(false)
@@ -115,19 +95,55 @@ class CricketActivity : AppCompatActivity() {
             builder.setTitle(R.string.jerseyTitle)
             builder.setMessage(R.string.jersey_details)
             builder.setIcon(android.R.drawable.ic_dialog_info)
-            builder.setPositiveButton(R.string.addedItem){dialogInterface, which ->
-                Toast.makeText(applicationContext,"Item Added",Toast.LENGTH_LONG).show()
+            builder.setPositiveButton(R.string.addedItem) { dialogInterface, which ->
+                Toast.makeText(applicationContext, "Item Added", Toast.LENGTH_LONG).show()
             }
-            builder.setNeutralButton("Cancel"){dialogInterface , which ->
-                Toast.makeText(applicationContext,R.string.cancelString,Toast.LENGTH_LONG).show()
+            builder.setNeutralButton("Cancel") { dialogInterface, which ->
+                Toast.makeText(applicationContext, R.string.cancelString, Toast.LENGTH_LONG).show()
             }
             val alertDialog: AlertDialog = builder.create()
             alertDialog.setCancelable(false)
             alertDialog.show()
         }
-        val textMarquee= binding.marqueeText
-        textMarquee.isSelected=true
-        textMarquee.isSingleLine=true
+        val textMarquee = binding.marqueeText
+        textMarquee.isSelected = true
+        textMarquee.isSingleLine = true
+            binding.minusButton.setOnClickListener {
+                quantity--
+                if (quantity <= 0) {
+                    Log.d("Cricket", "quantity is less than 0")
+                    val toast = Toast.makeText(
+                        applicationContext, R.string.quantityToast, Toast.LENGTH_SHORT
+                    )
+                    quantity = 1
+                }
+                binding.quantity.text = quantity.toString()
+            }
+            binding.plusButton.setOnClickListener {
+                quantity++
+                binding.quantity.text = quantity.toString()
+            }
+
+        binding.golves.setOnClickListener {
+            Toast.makeText(applicationContext, R.string.itemSelectToast, Toast.LENGTH_LONG).show()
+            productCost = 460
+        }
+        binding.confirm.setOnClickListener {
+            bill(productCost)
+            binding.cost.text = "Quantity $quantity gloves \nCost "+ totalCost.toString()
+
+        }
 
     }
+
+    fun bill( productCost:Int)
+    {
+        Log.i("Cricket", "$quantity")
+        totalCost= productCost * quantity
+        Log.i("Cricket", "total amount $totalCost")
+
+    }
+
+
+
 }
